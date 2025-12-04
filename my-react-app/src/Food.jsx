e0c2d214b08013e7824729bf9270b0ad64a5ea9f
Layanjala <layanjalajayasundara@gmail.com>
2025-12-04 20:51:34 +0530

    initial commit

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,12 @@
+function Food() {
+	const food1 = "Pizza";
+	const food2 = "Sushi";
+	return (
+		<ul>
+			<li>Apple</li>
+			<li>{food1.toUpperCase()}</li>
+			<li>{food2}</li>
+		</ul>
+	);
+}
+export default Food;
