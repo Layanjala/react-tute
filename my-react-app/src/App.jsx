e1d0d0f2aa2a991ec4365673f78652deebcbd0e2
Layanjala <layanjalajayasundara@gmail.com>
2025-12-15 22:15:59 +0530

    router

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,26 +12,32 @@ import OnChange from "./OnChange.jsx";
 import ColourPicker from "./ColourPicker.jsx";
 import UpdateArray from "./UpdateArray.jsx";
 import ToDoList from "./ToDoList.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div>
-			<Header />
-			<Food />
-			<Card />
-			<Button />
-			<Student name="John Doe" age={15} />
-			<Student name="Patric Harvy" age={42} />
-			<Student />
-			<UserGreeting isloggedin={true} username="Laya" />
-			<List />
-			<ProfilePicture />
-			<MyComponent />
-			<OnChange />
-			<ColourPicker />
-			<UpdateArray />
-			<ToDoList />
-			<Footer />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Food />}>
+						<Header />
+						<Card />
+						<Button />
+						<Student name="John Doe" age={15} />
+						<Student name="Patric Harvy" age={42} />
+						<Student />
+						<UserGreeting isloggedin={true} username="Laya" />
+						<List />
+						<ProfilePicture />
+						<MyComponent />
+						<OnChange />
+						<ColourPicker />
+						<UpdateArray />
+						<ToDoList />
+						<Footer />
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
